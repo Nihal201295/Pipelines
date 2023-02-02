fa4766acf394ec5e3dc903c2d15b17d3d7bb3e2c
Nihal201295 <106063609+Nihal201295@users.noreply.github.com>
2023-02-02 06:11:23 +0530

    New Update

--- Tomcat-Deployment-with-S3-Artifact.groovy
+++ Tomcat-Deployment-with-S3-Artifact.groovy
@@ -4,9 +4,19 @@ pipeline {
     }
 
     stages {
-        stage('Code-pulley-') {
+        stage('Code-pull') {
             steps {
                 git 'https://github.com/Nihal201295/onlinebookstore.git'
+            }
+        }
+    }
+    stages {
+        stage('Code-Buid') {
+            steps {
+                sh 'sudo apt-get update -y'
+                sh 'sudo apt-get install maven -y'
+                sh 'mvn --version'
+                sh 'mvn clean package'
             }
         }
     }
